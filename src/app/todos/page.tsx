58a47926226d1d8f711d244033fb2d093c488141
chae <yce0222@gmail.com>
2025-04-22 11:29:24 +0900

    feat: create todolist page

---
+++ src/app/todos/page.tsx
@@ -0,0 +1,11 @@
+// src/app/todos/page.tsx
+import TodoList from '../components/TodoList';
+
+export default function TodosPage() {
+  return (
+    <main>
+      <h1>Todos 페이지</h1>
+      <TodoList />
+    </main>
+  );
+}
